f28043f9d1fba34c7df7421b04c6b409f36d97dc
gustavo-candido <gustavocandidofreitas@gmail.com>
2020-12-05 02:05:09 -0300

    Add thread para ficar salvando no disco a cada 3 segundos

--- trabalho/src/main/java/SGBD/Ruler.java
+++ trabalho/src/main/java/SGBD/Ruler.java
@@ -1,24 +1,47 @@
 package SGBD;
 
+import SGBD.storage.ReadDiskService;
+import SGBD.storage.StoreDiskService;
 import com.proto.baiacu.*;
 
+import java.io.IOException;
 import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.Future;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Semaphore;
-import java.util.concurrent.TimeUnit;
 
 public class Ruler {
-    private int MAX_T = 3;
-    private HashMap<Key, Value> data = new HashMap<>();
+    private int MAX_T = 8;
+    private String FILE_PATH = "src/main/java/SGBD/storage/baiacu-disk-data";
+    private long DISK_STORAGE_PERIOD = 3 * 1000; // 3 seg
+    private HashMap<Key, Value> data;
     private ExecutorService pool = Executors.newFixedThreadPool(MAX_T);
     Semaphore sem = new Semaphore(1);
 
 
     public HashMap<Key, Value> getData() {
         return data;
+    }
+
+    public Ruler() throws IOException, ClassNotFoundException {
+        ReadDiskService readDiskService = new ReadDiskService();
+        data = (HashMap<Key, Value>) readDiskService.run(FILE_PATH);
+
+        new Timer().scheduleAtFixedRate(new TimerTask(){
+            @Override
+            public void run(){
+                StoreDiskService storeDiskService = new StoreDiskService();
+                try {
+                    storeDiskService.run(data, FILE_PATH);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        },0,DISK_STORAGE_PERIOD);
     }
 
     public void setData(
@@ -31,6 +54,9 @@ public class Ruler {
         Future<StoreResponse> submit = pool.submit(new WorkerStore(data, request));
         try {
             StoreResponse storeResponse = submit.get();
+
+
+
             sem.release();
             return storeResponse;
         } catch (InterruptedException | ExecutionException e) {
@@ -66,9 +92,6 @@ public class Ruler {
             sem.release();
             return null;
         }
-
-
-
     }
 
     public DestroyByVersionResponse destroyByVersionHandler(DestroyByVersionRequest request)
